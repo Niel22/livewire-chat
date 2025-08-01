25667e114c6a67610ec290fb8ba3516626cd72cd
Niel22 <niel2264@gmail.com>
2025-08-01 09:19:51 +0100

    Initial Commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,6 @@ class User extends Authenticatable
     }
 
     public function conversations(){
-        return $this->hasMany(Conversation::class, 'sender_id')->orWhere('receiver_id', $this->id)->latest();
+        return $this->hasMany(Conversation::class, 'sender_id')->orWhere('receiver_id', $this->id)->latest('updated_at');
     }
 }
