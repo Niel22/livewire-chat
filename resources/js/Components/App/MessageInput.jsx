b030a543a897d538c75977d0ec7abd6b459d61ce
Niel22 <niel2264@gmail.com>
2025-09-22 00:20:18 +0100

    Push

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -156,100 +156,148 @@ const MessageInput = ({conversation = null, setReplyingTo, replyingTo, user, isL
   }
 
   return (
-    <div className='max-w-full mt-auto flex flex-wrap items-end justify-start z-100 py-3 overflow-x-hidden'>
-      <div className=" px-1 xs:p-0 min-w-md basis-full flex-1 relative">
-
-        {chosenFiles?.length > 0 && !!uploadProgress && (
-          <progress className='progress progress-info w-full' value={uploadProgress} max="100" />
-        )}
-
-        {replyingTo && (
-          <ReplyToMessage setReplyingTo={setReplyingTo} replyingTo={replyingTo} />
-        )}
-
-        <div className='flex flex-nowrap gap-1 px-2 py-2'>
-          {chosenFiles?.map((file) => (
-            <div key={file.file.name} className={`relative flex justify-between cursor-pointer ` + (!isImage(file.file) ? " w-[240px]" : "")}>
-
-              {isImage(file.file) && (
-                <img src={file.url} alt="" className='w-16 h-16 object-cover' />
+      <div className="max-w-full mt-auto flex flex-wrap items-end justify-start z-100 py-3 overflow-x-hidden">
+          <div className=" px-1 xs:p-0 min-w-md basis-full flex-1 relative">
+              {chosenFiles?.length > 0 && !!uploadProgress && (
+                  <progress
+                      className="progress progress-info w-full"
+                      value={uploadProgress}
+                      max="100"
+                  />
               )}
 
-              {isAudio(file.file) && (
-                <CustomAudioPlayer file={file} showVolume={false} />
+              {replyingTo && (
+                  <ReplyToMessage
+                      setReplyingTo={setReplyingTo}
+                      replyingTo={replyingTo}
+                  />
               )}
 
-              {!isAudio(file.file) && !isImage(file.file) && (
-                <AttachmentPreview file={file} />
+              <div className="flex flex-nowrap gap-1 px-2 py-2">
+                  {chosenFiles?.map((file) => (
+                      <div
+                          key={file.file.name}
+                          className={
+                              `relative flex justify-between cursor-pointer ` +
+                              (!isImage(file.file) ? " w-[240px]" : "")
+                          }
+                      >
+                          {isImage(file.file) && (
+                              <img
+                                  src={file.url}
+                                  alt=""
+                                  className="w-16 h-16 object-cover"
+                              />
+                          )}
+
+                          {isAudio(file.file) && (
+                              <CustomAudioPlayer
+                                  file={file}
+                                  showVolume={false}
+                              />
+                          )}
+
+                          {!isAudio(file.file) && !isImage(file.file) && (
+                              <AttachmentPreview file={file} />
+                          )}
+
+                          <button
+                              onClick={() =>
+                                  setChosenFiles((prev) =>
+                                      prev.filter(
+                                          (f) => f.file.name !== file.file.name
+                                      )
+                                  )
+                              }
+                              className="absolute w-6 h-6 rounded-full bg-gray-800 -right-2 -top-2 text-gray-300 hover:text-gray-100 z-10"
+                          >
+                              <XCircleIcon className="w-6" />
+                          </button>
+                      </div>
+                  ))}
+              </div>
+
+              {!isGroupLocked() && (
+                  <div className="border dark:border-slate-700 border-slate-300 shadow-md mx-2 rounded-full flex gap-1 items-end px-4 py-2">
+                      <button className="p-1 text-gray-400 hover:text-gray-300 relative">
+                          <PaperClipIcon className="w-6" />
+                          <input
+                              type="file"
+                              onChange={onFileChange}
+                              multiple
+                              className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
+                          />
+                      </button>
+
+                      <button className="p-1 text-gray-400 hover:text-gray-300 cursor-pointer relative">
+                          <PhotoIcon className="w-6" />
+                          <input
+                              type="file"
+                              onChange={onFileChange}
+                              accept="image/*"
+                              multiple
+                              className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
+                          />
+                      </button>
+                      
+                      <NewMessageInput
+                          value={newMessage}
+                          onSend={onSendClick}
+                          onChange={(e) => setNewMessage(e.target.value)}
+                          onPaste={handlePaste}
+                      />
+
+                      {/* <Popover className="relative">
+                            <PopoverButton className="p-1 focus:outline-none focus:border-none text-gray-400 hover:text-gray-300">
+                              <FaceSmileIcon className='w-6 h-6' />
+                            </PopoverButton>
+                            <PopoverPanel className="absolute left-0 bottom-full">
+                              <EmojiPicker theme='dark' onEmojiClick={e => setNewMessage(newMessage + e.emoji)} />
+                            </PopoverPanel>
+                          </Popover> */}
+
+                      {newMessage || chosenFiles?.length > 0 ? (
+                          <button
+                              onClick={onSendClick}
+                              disabled={messageSending}
+                              className="py-2 px-3 rounded-md bg-blue-500"
+                          >
+                              {messageSending ? (
+                                  <span className="loading loading-dots loading-md"></span>
+                              ) : (
+                                  <PaperAirplaneIcon className="w-6" />
+                              )}
+                          </button>
+                      ) : (
+                          <button
+                              disabled={messageSending}
+                              className=" py-2 px-3 rounded-md bg-blue-500 p-2"
+                          >
+                              <HandThumbUpIcon
+                                  onClick={onLikeClick}
+                                  className="w-6 h-6"
+                              />
+                          </button>
+                      )}
+                  </div>
               )}
 
-              <button onClick={ () => 
-                setChosenFiles(prev => prev.filter(f => f.file.name !== file.file.name))
-              } className='absolute w-6 h-6 rounded-full bg-gray-800 -right-2 -top-2 text-gray-300 hover:text-gray-100 z-10'>
-                <XCircleIcon className='w-6' />
-              </button>
-            </div>
-          ))}
-        </div>
+              {isGroupLocked() && (
+                  <div className="border dark:border-slate-700 border-slate-300 shadow-md mx-2 rounded-lg flex items-center gap-2 px-4 py-3 bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-300">
+                      <LockClosedIcon className="w-6 h-6 text-gray-500 dark:text-gray-400" />
+                      <span className="text-sm">
+                          This group is locked. You cannot send messages at the
+                          moment.
+                      </span>
+                  </div>
+              )}
 
-        
-        {!isGroupLocked() && (
-          <div className="border dark:border-slate-700 border-slate-300 shadow-md mx-2 rounded-full flex gap-1 items-end px-4 py-2">
-            {/* <Popover className="relative">
-              <PopoverButton className="p-1 focus:outline-none focus:border-none text-gray-400 hover:text-gray-300">
-                <FaceSmileIcon className='w-6 h-6' />
-              </PopoverButton>
-              <PopoverPanel className="absolute left-0 bottom-full">
-                <EmojiPicker theme='dark' onEmojiClick={e => setNewMessage(newMessage + e.emoji)} />
-              </PopoverPanel>
-            </Popover> */}
-
-            <NewMessageInput
-              value={newMessage}
-              onSend={onSendClick}
-              onChange={(e) => setNewMessage(e.target.value)}
-              onPaste={handlePaste}
-            />
-
-            <button className="p-1 text-gray-400 hover:text-gray-300 relative">
-              <PaperClipIcon className='w-6' />
-              <input type="file" onChange={onFileChange} multiple className='absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer' />
-            </button>
-
-            <button className="p-1 text-gray-400 hover:text-gray-300 cursor-pointer relative">
-              <PhotoIcon className='w-6' />
-              <input type="file" onChange={onFileChange} accept='image/*' multiple className='absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer' />
-            </button>
-
-            {newMessage || chosenFiles?.length > 0 ? (
-              <button onClick={onSendClick} disabled={messageSending} className='py-2 px-3 rounded-md bg-blue-500'>
-                {messageSending ? (<span className="loading loading-dots loading-md"></span>) : (
-                  <PaperAirplaneIcon className='w-6' />
-                )}
-              </button>
-            ) : (
-              <button disabled={messageSending} className=' py-2 px-3 rounded-md bg-blue-500 p-2'>
-                <HandThumbUpIcon onClick={onLikeClick} className='w-6 h-6' />
-              </button>
-            )}
-          </div>
-        )}
-
-        {isGroupLocked() && (
-          <div className="border dark:border-slate-700 border-slate-300 shadow-md mx-2 rounded-lg flex items-center gap-2 px-4 py-3 bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-300">
-            <LockClosedIcon className="w-6 h-6 text-gray-500 dark:text-gray-400" />
-            <span className="text-sm">
-              This group is locked. You cannot send messages at the moment.
-            </span>
-          </div>
-        )}
-        
-        {/* {inputErrorMessage && (
+              {/* {inputErrorMessage && (
           <p className='text-xs text-center text-red-400'>{inputErrorMessage}</p>
         )} */}
+          </div>
       </div>
-    </div>
-  )
+  );
 }
 
 export default MessageInput
