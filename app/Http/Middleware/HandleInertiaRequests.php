23407c2fb5aa2d42cee3eedacf00c31108842f7e
Niel22 <niel2264@gmail.com>
2025-10-26 15:45:39 +0100

    fix: optimize the response time of the platform nd image upload

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,27 +34,26 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         $user = $request->user();
+
+        if ($user) {
+            if ($user->role === 'staff') {
+                $subAccounts = User::where('staff_id', $user->id)
+                    ->orWhere('id', $user->id)
+                    ->get();
+            } elseif ($user->role === 'member' && $user->staff_id) {
+                $subAccounts = User::where('staff_id', $user->staff_id)
+                    ->orWhere('id', $user->staff_id)
+                    ->get();
+            }
+            $subAccounts = $subAccounts->unique('id')->values();
+        }
+
         return [
             ...parent::share($request),
             'auth' => [
                 'user' => $user ? new UserResource($request->user()) : null,
             ],
-            'sub_account' => $user
-                ? User::query()
-                    ->when($user->role === 'staff', function ($query) use ($user) {
-                        $query->where('staff_id', $user->id)
-                            ->orWhere('id', $user->id);
-                    })
-                    ->when($user->role === 'member' && $user->staff_id, function ($query) use ($user) {
-                        $query->where(function ($q) use ($user) {
-                            $q->where('staff_id', $user->staff_id)
-                            ->orWhere('id', $user->staff_id);
-                        });
-                    })
-                    ->get()
-                    ->unique('id')
-                    ->values()
-                : []
+            'sub_account' => $subAccounts,
         ];
     }
 }
