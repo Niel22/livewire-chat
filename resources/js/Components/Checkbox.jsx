ed0b7fe1a1361d3face5a81fee46b922a7267d88
Niel22 <niel2264@gmail.com>
2025-10-27 09:17:34 +0100

    checking fi

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -7,6 +7,7 @@ export default function Checkbox({ className = '', ...props }) {
                 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 ' +
                 className
             }
+            
         />
     );
 }
